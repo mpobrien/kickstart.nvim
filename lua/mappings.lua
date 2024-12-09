a4b97cf5e88b0272155b051d2d4b314d9905e594
Michael O'Brien <mpobrien005@gmail.com>
2024-12-08 22:13:39 -0500

    add mapping for moving lines up/down

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,5 +8,13 @@ map('n', '<leader>h', function()
 end, { desc = 'Toggle inlay hints' })
 
 -- Toggling comments
-map('n', '<leader>/', ':norm gcc<CR>', { remap = true }, { desc = 'Toggle comments' })
-map('v', '<leader>/', 'gc', { remap = true }, { desc = 'Toggle comments' })
+map('n', '<leader>/', ':norm gcc<CR>', { remap = true, desc = 'Toggle comments' })
+map('v', '<leader>/', 'gc', { remap = true, desc = 'Toggle comments' })
+
+-- Alt + jk to move line up/down
+vim.keymap.set('n', '<A-j>', ':m .+1<cr>==', { noremap = true, silent = true, desc = 'Move line down' })
+vim.keymap.set('n', '<A-k>', ':m .-2<cr>==', { noremap = true, silent = true, desc = 'Move line up' })
+vim.keymap.set('i', '<A-j>', '<Esc>:m .+1<cr>==gi', { noremap = true, silent = true, desc = 'Move line down (insert mode)' })
+vim.keymap.set('i', '<A-k>', '<Esc>:m .-2<cr>==gi', { noremap = true, silent = true, desc = 'Move line up (insert mode)' })
+vim.keymap.set('x', '<A-j>', ":m '>+1<cr>gv=gv", { noremap = true, silent = true, desc = 'Move block down' })
+vim.keymap.set('x', '<A-k>', ":m '<-2<cr>gv=gv", { noremap = true, silent = true, desc = 'Move block up' })
